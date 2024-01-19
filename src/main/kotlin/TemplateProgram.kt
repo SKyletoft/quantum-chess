6ed091eb2329833516c8629c23a87b2b4253c8b1
Samuel Kyletoft <samuel@kyletoft.se>
2024-01-19 12:54:09 +0100

    Sprite drawing

--- src/main/kotlin/TemplateProgram.kt
+++ src/main/kotlin/TemplateProgram.kt
@@ -1,12 +1,14 @@
 import org.openrndr.application
-import org.openrndr.color.ColorRGBa
 import org.openrndr.draw.Drawer
+import org.openrndr.draw.loadImage
 import org.openrndr.drawImage
 import org.openrndr.math.Vector2
+import org.openrndr.shape.Rectangle
+import org.openrndr.color.ColorRGBa as ColourRGBa
 
 val drawChessBoard: Drawer.() -> Unit = {
-	this.clear(ColorRGBa.fromHex(0xECD3B9))
-	this.fill = ColorRGBa.fromHex(0xA16F5A)
+	this.clear(ColourRGBa.fromHex(0xECD3B9))
+	this.fill = ColourRGBa.fromHex(0xA16F5A)
 	val squares =
 		listOf(
 			Vector2(0.0, 0.0),
@@ -51,7 +53,34 @@ val drawChessBoard: Drawer.() -> Unit = {
 	// Beacuse apparently 0 still draws an outline?
 	this.strokeWeight = -1.0
 	this.rectangles(squares, this.context.width.toDouble() / 8, this.context.height.toDouble() / 8)
+	this.strokeWeight = 1.0
 }
+
+fun getSprite(
+	x: Int,
+	y: Int,
+): Rectangle {
+	val spriteSize = 334.0
+	return Rectangle(
+		x * spriteSize,
+		y * spriteSize,
+		spriteSize,
+		spriteSize,
+	)
+}
+
+val whiteKing   = getSprite(0, 0)
+val whiteQueen  = getSprite(1, 0)
+val whiteBishop = getSprite(2, 0)
+val whiteKnight = getSprite(3, 0)
+val whiteCastle = getSprite(4, 0)
+val whitePawn   = getSprite(5, 0)
+val blackKing   = getSprite(0, 1)
+val blackQueen  = getSprite(1, 1)
+val blackBishop = getSprite(2, 1)
+val blackKnight = getSprite(3, 1)
+val blackCastle = getSprite(4, 1)
+val blackPawn   = getSprite(5, 1)
 
 fun main() =
 	application {
@@ -62,9 +91,18 @@ fun main() =
 
 		program {
 			val board = drawImage(drawer.context.width, drawer.context.height) { drawChessBoard(drawer) }
+			val pieces = loadImage("data/images/pieces.png")
 
 			extend {
+				val w = drawer.context.width.toDouble() / 8.0
+				val h = drawer.context.height.toDouble() / 8.0
 				drawer.image(board)
+
+				drawer.drawStyle.strokeWeight = 1.0
+				drawer.fill = ColourRGBa.BLACK
+				drawer.image(pieces, whiteKing, Rectangle(0.0, 0.0, w, h))
+				drawer.image(pieces, whiteQueen, Rectangle(160.0, 0.0, w, h))
+				drawer.image(pieces, blackKing, Rectangle(80.0, 0.0, w, h))
 			}
 		}
 	}
