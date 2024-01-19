ffbd28089bde29750956c5174e848c18a70e06ac
Samuel Kyletoft <samuel@kyletoft.se>
2024-01-19 12:13:03 +0100

    Draw a chessboard

--- src/main/kotlin/TemplateProgram.kt
+++ src/main/kotlin/TemplateProgram.kt
@@ -1,36 +1,70 @@
 import org.openrndr.application
 import org.openrndr.color.ColorRGBa
-import org.openrndr.draw.loadFont
-import org.openrndr.draw.loadImage
-import org.openrndr.draw.tint
-import kotlin.math.cos
-import kotlin.math.sin
+import org.openrndr.draw.Drawer
+import org.openrndr.drawImage
+import org.openrndr.math.Vector2
+
+val drawChessBoard: Drawer.() -> Unit = {
+	this.clear(ColorRGBa.fromHex(0xECD3B9))
+	this.fill = ColorRGBa.fromHex(0xA16F5A)
+	val squares =
+		listOf(
+			Vector2(0.0, 0.0),
+			Vector2(2.0, 0.0),
+			Vector2(4.0, 0.0),
+			Vector2(6.0, 0.0),
+			Vector2(1.0, 1.0),
+			Vector2(3.0, 1.0),
+			Vector2(5.0, 1.0),
+			Vector2(7.0, 1.0),
+			Vector2(0.0, 2.0),
+			Vector2(2.0, 2.0),
+			Vector2(4.0, 2.0),
+			Vector2(6.0, 2.0),
+			Vector2(1.0, 3.0),
+			Vector2(3.0, 3.0),
+			Vector2(5.0, 3.0),
+			Vector2(7.0, 3.0),
+			Vector2(0.0, 4.0),
+			Vector2(2.0, 4.0),
+			Vector2(4.0, 4.0),
+			Vector2(6.0, 4.0),
+			Vector2(1.0, 5.0),
+			Vector2(3.0, 5.0),
+			Vector2(5.0, 5.0),
+			Vector2(7.0, 5.0),
+			Vector2(0.0, 6.0),
+			Vector2(2.0, 6.0),
+			Vector2(4.0, 6.0),
+			Vector2(6.0, 6.0),
+			Vector2(1.0, 7.0),
+			Vector2(3.0, 7.0),
+			Vector2(5.0, 7.0),
+			Vector2(7.0, 7.0),
+		).map {
+			Vector2(
+				it.x * this.context.width.toDouble() / 8,
+				it.y * this.context.height.toDouble() / 8,
+			)
+		}
+
+	// Beacuse apparently 0 still draws an outline?
+	this.strokeWeight = -1.0
+	this.rectangles(squares, this.context.width.toDouble() / 8, this.context.height.toDouble() / 8)
+}
 
 fun main() =
 	application {
 		configure {
-			width = 768
-			height = 576
+			width = 640
+			height = 640
 		}
 
 		program {
-			val image = loadImage("data/images/pm5544.png")
-			val font = loadFont("data/fonts/default.otf", 64.0)
+			val board = drawImage(drawer.context.width, drawer.context.height) { drawChessBoard(drawer) }
 
 			extend {
-				drawer.drawStyle.colorMatrix = tint(ColorRGBa.WHITE.shade(0.2))
-				drawer.image(image)
-
-				drawer.fill = ColorRGBa.PINK
-				drawer.circle(
-					cos(seconds) * width / 2.0 + width / 2.0,
-					sin(0.45 * seconds) * height / 2.0 + height / 2.0,
-					140.0,
-				)
-
-				drawer.fontMap = font
-				drawer.fill = ColorRGBa.WHITE
-				drawer.text("OPENRNDR", width / 2.0, height / 2.0)
+				drawer.image(board)
 			}
 		}
 	}
